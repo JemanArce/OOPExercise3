aa6a6a8d7a7b480a47c447526a122ff8657acc59
Jedidiah Emmanuel G. Arce <116769906+JemanArce@users.noreply.github.com>
2022-12-14 14:02:20 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main
+{
+	public static void main(String[] args){
+		Motor Honda = new Motor("Orange");
+		System.out.println(Honda.start());
+	}
+}
---
+++ MotorEngine.java
@@ -0,0 +1,9 @@
+class MotorEngine
+{
+	public String startMotorEngine(){
+		return" The Motor Engine has started.";
+	}
+	public String stopMotorEngine(){
+		return"The Motoe Engine has Stopped.";
+	}
+}
